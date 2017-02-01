ca1a3a691d151c3980d1a3354c9460968b449381
Menedyl <nicolas.bostjancic@gmail.com>
2017-02-01 12:34:13 +0100

    Close #5 Creation de la vue pour ajouter une figure Creation du formulaire pour ajouter une figure Maj du CSS

---
+++ src/AppBundle/Form/FigureType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FigureType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('content', TextareaType::class)
+            ->add('rating', ChoiceType::class, array(
+                "choices" => array(
+                    'Novice' => 0,
+                    'Débutant' => 1,
+                    'Amateur' => 2,
+                    'Confirmé' => 3,
+                    'Expert' => 4,
+                    "Pro" => 5
+
+            )))
+            ->add('groupFigure', ChoiceType::class, array(
+                "choices" => array(
+                    'Groupe 1' => 'Groupe 1',
+                    'Groupe 2' => 'Groupe 2',
+                    'Groupe 3' => 'Groupe 3'
+                )
+            ))
+            ->add('Valider', SubmitType::class)
+        ;
+
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Figure'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_figure';
+    }
+
+
+}
